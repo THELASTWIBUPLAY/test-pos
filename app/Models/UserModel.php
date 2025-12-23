cce3e2382aa41471b6dfc3fb1d7d159b796f95e6
Muhamad Masyhuri <masyhuri161003@gmail.com>
2025-12-23 12:38:47 +0700

    jobsheet-7 error users

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
@@ -14,11 +15,25 @@ class UserModel extends Model
     protected $table = 'm_user';       // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = ['level_id','username', 'nama', 'password'];
+    protected $fillable = ['level_id','username', 'nama', 'password', 'created_at', 'updated_at'];
+
+    protected $hidden = ['password'];
+
+    protected $casts = ['password' => 'hashed'];
 
     public function level() : BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function getRoleName() : string
+    {
+        return $this->level->level_nama;
+    }
+
+    public function hasRole($role) : bool
+    {
+        return $this->level->level_kode == $role;
     }
     
 }
